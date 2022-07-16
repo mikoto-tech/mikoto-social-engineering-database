aa256c87fe5247635957f8756043c128e6011157
mikoto <mikoto@ungine.cn>
2022-07-16 12:10:26 +0800

    [init]: Init repo

---
+++ src/main/java/net/mikoto/socialEngineering/database/SocialEngineeringDatabaseApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.socialEngineering.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialEngineeringDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialEngineeringDatabaseApplication.class, args);
+	}
+
+}
